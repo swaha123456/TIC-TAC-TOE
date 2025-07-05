9c224e4e2040846e619848cc8809c5256a531b60
swaha123456 <swaharoy1@gmail.com>
2025-07-06 02:02:35 +0530

    Update the file

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,37 @@
+  let boxes = document.querySelectorAll(".box");
+  let resetBtn = document.querySelector("#reset-btn");
+  let turnO = true;
+
+  const winarr = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 8],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box clicked");
+        if(turnO == true){
+            box.innerText = "O";
+            turnO = false;
+        }
+        else{
+             box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkwin();
+    });
+
+});
+
+const checkwin = () => {
+    
+}
+
